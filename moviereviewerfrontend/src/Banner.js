fbf08f4de46ad98df5de60aec940ebfe7fb35ddd
onurinc <Onur.i@hotmail.nl>
2021-10-19 10:55:02 +0200

    Changed some styling in the banner from .js to .css

--- moviereviewerfrontend/src/Banner.js
+++ moviereviewerfrontend/src/Banner.js
@@ -27,9 +27,7 @@ function Banner() {
     <header
       className="banner"
       style={{
-        backgroundSize: "cover",
         backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-        backgroundPosition: "center center",
       }}
     >
       <div className="banner__contents">
