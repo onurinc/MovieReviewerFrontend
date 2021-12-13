19a47f70dba7252259261d8b1ca5821ed7536116
onurinc <onur.i@hotmail.nl>
2021-12-13 03:26:21 +0100

    Implemented working Login function which saves the JWT token in the localstorage

--- moviereviewerfrontend/src/Pages/Login.js
+++ moviereviewerfrontend/src/Pages/Login.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import NavMenu from "../Components/NavMenu";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Form, Button } from "react-bootstrap";
 import "./Styling/Login.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const data = { email: email, password: password };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    fetch("https://localhost:44318/api/Accounts/Login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      //Then with the data from the response in JSON...
+      .then((data) => {
+        console.log("Success:", data);
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("loggedIn", "true");
+        window.location = "/";
+      })
+      //Then with the error genereted...
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   return (
     <>
       <NavMenu />
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -19,10 +50,14 @@ function Login() {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
 
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" onClick={submitHandler}>
           Submit
         </Button>
       </Form>
