8e00f2b693462c8e70545108f72286773b7c12ac
onurinc <onur.i@hotmail.nl>
2022-01-19 18:16:59 +0100

    Finished Actor page tests and Navbar tests

---
+++ moviereviewerfrontend/cypress/integration/ActorsPageTests.js
@@ -0,0 +1,32 @@
+it("Going to the actor page", function () {
+  /* ==== Generated with Cypress Studio ==== */
+  cy.visit("http://localhost:3000/");
+  /* ==== End Cypress Studio ==== */
+  /* ==== Generated with Cypress Studio ==== */
+  cy.get('[href="/actors"]').click();
+  /* ==== End Cypress Studio ==== */
+});
+
+it("Clicking on actors movie 1", function () {
+  /* ==== Generated with Cypress Studio ==== */
+  cy.visit("http://localhost:3000/actors");
+  /* ==== Generated with Cypress Studio ==== */
+  cy.get(':nth-child(2) > ul > :nth-child(1) > a').click();
+  /* ==== End Cypress Studio ==== */
+});
+
+it("Clicking on actors movie 2", function () {
+  /* ==== Generated with Cypress Studio ==== */
+  cy.visit("http://localhost:3000/actors");
+  /* ==== Generated with Cypress Studio ==== */
+  cy.get(':nth-child(2) > ul > :nth-child(2) > a').click();
+  /* ==== End Cypress Studio ==== */
+});
+
+it("Clicking on actors movie 2", function () {
+  /* ==== Generated with Cypress Studio ==== */
+  cy.visit("http://localhost:3000/actors");
+  /* ==== Generated with Cypress Studio ==== */
+  cy.get(':nth-child(2) > ul > :nth-child(3) > a').click();
+  /* ==== End Cypress Studio ==== */
+});
---
+++ moviereviewerfrontend/cypress/integration/NavTests.js
@@ -0,0 +1,35 @@
+it("clicking home page nav bar", function () {
+  /* ==== Generated with Cypress Studio ==== */
+  cy.visit("http://localhost:3000/");
+  /* ==== End Cypress Studio ==== */
+  /* ==== Generated with Cypress Studio ==== */
+  cy.get('[href="/"]').click();
+  /* ==== End Cypress Studio ==== */
+});
+
+it("clicking home page nav bar", function () {
+  /* ==== Generated with Cypress Studio ==== */
+  cy.visit("http://localhost:3000/");
+  /* ==== End Cypress Studio ==== */
+  /* ==== Generated with Cypress Studio ==== */
+  cy.get('[href="/actors"]').click();
+  /* ==== End Cypress Studio ==== */
+});
+
+it("clicking home page nav bar", function () {
+  /* ==== Generated with Cypress Studio ==== */
+  cy.visit("http://localhost:3000/");
+  /* ==== End Cypress Studio ==== */
+  /* ==== Generated with Cypress Studio ==== */
+  cy.get('[href="/login"]').click();
+  /* ==== End Cypress Studio ==== */
+});
+
+it("clicking home page nav bar", function () {
+  /* ==== Generated with Cypress Studio ==== */
+  cy.visit("http://localhost:3000/");
+  /* ==== End Cypress Studio ==== */
+  /* ==== Generated with Cypress Studio ==== */
+  cy.get('[href="/register"]').click();
+  /* ==== End Cypress Studio ==== */
+});
